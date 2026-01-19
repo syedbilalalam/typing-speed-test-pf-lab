d9413944b899060da4071770370f589b11b780a4
Syed Bilal Alam <syedbilalalam2006@gmail.com>
2026-01-19 06:15:32 +0500

    Refactor: Unused imports removed

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, session
 
 # Flask initialization code
 app = Flask(__name__)
